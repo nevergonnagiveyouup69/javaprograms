ddb540c38378448f55fe82e3205c2d0a4e6ea6b7
nevergonnagiveyouup69 <97106120+nevergonnagiveyouup69@users.noreply.github.com>
2022-08-22 21:27:29 +0530

    Add files via upload

---
+++ pgavg.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner read = new Scanner(System.in);
+        double score1 = read.nextDouble();
+        double score2 = read.nextDouble();
+        double score3 = read.nextDouble();
+        double score4 = read.nextDouble();
+        double avgScore = getAverageScore(score1, score2, score3, score4);
+        System.out.println(avgScore);
+    }
+
+    // create your method here
+    public static double getAverageScore(double a, double b, double c, double d) {
+        double res, sum;
+        sum = a + b + c + d;
+        res = sum / 4;
+        return res;
+    }
+}
