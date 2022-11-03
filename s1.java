d0f4b55ae8ea828bd630effe10d3d9b705d34f9a
Kaushal r <97106120+nevergonnagiveyouup69@users.noreply.github.com>
2022-11-03 19:00:59 +0530

    Add files via upload

---
+++ s1.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+class s1 {
+
+    public static void main(String args[]) {
+
+        try (Scanner sc = new Scanner(System.in)) {
+            System.out.println("enter size");
+            String a = sc.next();
+            String b = "";
+            for (int i = 0; i < a.length(); i++) {
+                if (a.charAt(i) == '@') {
+                    break;
+                } else {
+                    b += a.charAt(i);
+                }
+            }
+            System.out.println(b);
+        }
+        return;
+    }
+}
