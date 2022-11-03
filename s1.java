26d65dcac8fb3513391d7bd28e0afa73f859683e
Kaushal r <97106120+nevergonnagiveyouup69@users.noreply.github.com>
2022-11-03 20:17:23 +0530

    Update s1.java

--- s1.java
+++ s1.java
@@ -5,7 +5,7 @@ class s1 {
     public static void main(String args[]) {
 
         try (Scanner sc = new Scanner(System.in)) {
-            System.out.println("enter size");
+            System.out.println("enter email");
             String a = sc.next();
             String b = "";
             for (int i = 0; i < a.length(); i++) {
