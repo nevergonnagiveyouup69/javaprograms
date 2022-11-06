cb40a319c15d8c8544a153e654a7f7dfd55b71de
Kaushal r <97106120+nevergonnagiveyouup69@users.noreply.github.com>
2022-11-06 13:09:57 +0530

    Add files via upload

--- find_second_largest_element_in_an_arr.java
+++ find_second_largest_element_in_an_arr.java
@@ -5,11 +5,14 @@ class s1 {
     public static void sum(int a[], int b) {
         int first = Integer.MIN_VALUE, sec = Integer.MIN_VALUE;
         for (int i = 0; i < b; i++) {
-            if (a[i] > first) {
+            if (a[i] > first) { // greatest value
                 sec = first;
                 first = a[i];
             }
-            if (sec > a[i] || a[i] != first) {
+            if (sec > a[i] || a[i] != first) {/*
+                                               * second greatest value by if second value is greater than
+                                               * value first value arr value gets stored
+                                               */
                 sec = a[i];
             }
         }
@@ -20,9 +23,9 @@ class s1 {
     public static void main(String args[]) {
 
         Scanner sc = new Scanner(System.in);
-        System.out.println("enter size");
+        System.out.println("enter size:");
         int b = sc.nextInt();
-        System.out.println("enter the elmentsof the array:");
+        System.out.println("enter the elements of the array:");
         int a[] = new int[100];
         for (int i = 0; i < b; i++) {
             a[i] = sc.nextInt();
