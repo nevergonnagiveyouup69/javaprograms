c0897b01458cccf2e833b8eda73b574c64ed7dba
Kaushal r <97106120+nevergonnagiveyouup69@users.noreply.github.com>
2022-11-07 19:28:45 +0530

    Add files via upload

---
+++ pattern_upside_triangle.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+class s1 {
+
+    public static void main(String[] args) {
+        int i, j, n;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the no of lines");
+        n = sc.nextInt();
+        for (i = n; i >= 1; i--) {
+            for (j = i; j < n; j++)
+                System.out.print(" ");
+
+            for (j = 1; j <= (2 * i - 1); j++)
+                System.out.print("*");
+
+            System.out.println("");
+        }
+    }
+
+}
