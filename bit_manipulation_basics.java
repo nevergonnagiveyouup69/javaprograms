b116135c0c5a15d8ef3cb11254a080f59e158ba3
Kaushal r <97106120+nevergonnagiveyouup69@users.noreply.github.com>
2022-11-09 19:35:39 +0530

    Add files via upload

---
+++ bit_manipulation_basics.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+class s1 {
+
+    public static void sum(int a) {
+        int i = 0;
+        int bitmask = 1 << i;
+        int notbitmask = ~(bitmask);
+        int newnumber = bitmask | a;
+        System.out.println(bitmask + " " + notbitmask + " " + newnumber);
+        return;
+    }
+
+    public static void main(String args[]) {
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter elements:");
+        int a = sc.nextInt();
+        sum(a);
+    }
+}
