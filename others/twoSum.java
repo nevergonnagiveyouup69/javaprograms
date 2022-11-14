ac09b10b1b540e7a9a294e82969eccb1142f807c
Kaushal r <97106120+nevergonnagiveyouup69@users.noreply.github.com>
2022-11-14 21:17:00 +0530

    Add files via upload

---
+++ others/twoSum.java
@@ -0,0 +1,22 @@
+class twoSum {
+    public static int[] twoSum(int[] nums, int target) {
+
+        int max = nums.length;
+
+        for (int i = 0; i < max; i++) {
+            int diff = target - nums[i];
+            for (int j = i + 1; j < max; j++) {
+                if (nums[j] == diff) {
+                    return new int[] { i, j };
+                }
+            }
+        }
+        return new int[] { 0, 0 };
+    }
+
+    public static void main(String[] args) {
+        int arr[] = { 1, 2, 3 };
+        int tar = 3;
+        twoSum(arr, tar);
+    }
+}
