d5038f9888600787e888333f530bc7bb44b6bd80
Kaushal r <97106120+nevergonnagiveyouup69@users.noreply.github.com>
2022-11-18 21:33:03 +0530

    Add files via upload

---
+++ recursion/x_to_the_power_of_n.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+class s1 {
+
+    public static int sum(int x, int n) {
+
+        if (n == 0) {
+            return 1;
+        }
+        if (x == 0) {
+            return 0;
+        }
+        int pownm1 = sum(x, n - 1);
+        int xpown = x * pownm1;
+        return xpown;
+    }
+
+    public static void main(String args[]) {
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter value:");
+        int x = sc.nextInt();
+        int n = sc.nextInt();
+        int ans = sum(x, n);
+        System.out.println(ans);
+    }
+}
