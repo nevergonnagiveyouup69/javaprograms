1c9ebdbba8e1814ca07139dcf39673c4e4bf3813
Kaushal r <97106120+nevergonnagiveyouup69@users.noreply.github.com>
2022-11-23 22:38:29 +0530

    Update ascii.java

--- ascii.java
+++ ascii.java
@@ -14,7 +14,7 @@ class s1 {
     public static void main(String args[]) {
 
         Scanner sc = new Scanner(System.in);
-        System.out.println("enter elements:");
+        System.out.println("enter elements that have to be ascii:");
         String a = sc.next();
         sum(a);
     }
